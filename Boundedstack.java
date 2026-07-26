8524e804c36eb87d0294764384907b8ad93a5f31
suphawit-mahamong <suphawit.m@ku.th>
2026-07-26 10:49:27 +0700

    version 0.1

--- Boundedstack.java
+++ Boundedstack.java
@@ -1,3 +1,33 @@
+import java.util.*;
+
+/**
+ * 
+ * Boundedstack คือ 
+ */
 public class Boundedstack {
     
+    private final List<String> elements;
+    private final int capacity;
+
+    //AF(elements,capacity) =
+    //RI
+    //
+    //
+
+    /**
+     * 
+     * @param capacity
+     */
+    public Boundedstack(int capacity) {
+        elements = new ArrayList<>();
+        this.capacity = capacity;
+    }
+
+    /**
+     * 
+     * @param S
+     */
+    public void push(String S){
+
+    }
 }
--- Boundedstacktest.java
+++ Boundedstacktest.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class Boundedstacktest {
+    
+    private static int passed = 0;
+    private static int failed = 0;
+
     public static void main(String[] args) {
         
     }
